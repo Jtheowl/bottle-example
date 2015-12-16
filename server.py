27e80a984dc894326a061048a3bbda667d397a94
Jtheowl <jadon.combs@gmail.com>
2015-12-16 11:58:35 -0800

    added "ping" route

--- server.py
+++ server.py
@@ -2,9 +2,13 @@ import bottle
 
 APP = bottle.default_app()
 
+@APP.route('/')
+def index():
+  return '<p>Message changed by Jtheowl</p>'
+
 @APP.route('/ping')
 def ping():
-  return '<p>ok</p>'
+  return '<p>okay</p>'
 
 if __name__ == '__main__':
   bottle.run(application=APP)
